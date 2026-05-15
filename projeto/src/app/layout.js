9d57af7d1e7b3c89f77f46f0246bf66a64db3ff0
yulialejandra27 <Yuliannyalejandra27@gmail.com>
2026-05-15 14:06:29 +0000

    new file:   package-lock.json 	modified:   projeto/src/app/layout.js 	renamed:    projeto/src/app/favicon.ico -> projeto/src/app/logo.png.ico

--- projeto/src/app/layout.js
+++ projeto/src/app/layout.js
@@ -4,6 +4,9 @@ import Link from "next/link";
 export const metadata = {
   title: "Mural da Comunidade",
   description: "Plataforma colaborativa para eventos e vagas locais",
+  icons: {
+    icon: "/logo.png",
+  },
 };
 
 export default function RootLayout({ children }) {
